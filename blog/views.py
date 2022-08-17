c76e910c07a00c251fe82254fd98c16bef6eed07
pagioro <pagioro@hotmail.com>
2022-08-17 20:50:01 +0000

    post_detail.html

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Post
 
 
@@ -8,6 +8,7 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by("-created_on")
     template_name = "index.html"
     paginate_by = 6
+
 
 class PostDetail(View):
 
