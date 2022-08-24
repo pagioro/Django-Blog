6d74c7c8f1866a157deb7c3c06c7dfc8d17ad472
pagioro <pagioro@hotmail.com>
2022-08-24 21:04:43 +0000

    Add ALLOWED_HOSTS = ["codestarproject2022.herokuapp.com", "localhost"]

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["mb2test.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["codestarproject2022.herokuapp.com", "localhost"]
 
 # Application definition
 
